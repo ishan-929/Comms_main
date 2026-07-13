735d189a285889c6da1ff102752c6cc40fcf7344
Ishan Karmakar <karmakarbablu067@gmail.com>
2026-07-12 21:50:34 -0400

    MAC Address Getter fixed

--- MAC_GETTER_CODE/mac_getter.cpp
+++ MAC_GETTER_CODE/mac_getter.cpp
@@ -4,11 +4,9 @@
 void setup() {
     Serial.begin(115200);
     delay(1000);
-
     WiFi.mode(WIFI_STA);
-    Serial1.println("MAC Address: " + WiFi.macAddress());
+    Serial.println("MAC Address: " + WiFi.macAddress());
 }
 void loop() {
 
 }
-
